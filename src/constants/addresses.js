e9f7a65a8befb8b0083d465b0dd4512f3c3539cd
Anton Cheng <anton@coolbitx.com>
2020-02-23 00:13:52 -0800

    Move abi files

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,4 +1,5 @@
 export const mainnet = {
   ocDAI: '0xddac4aed7c8f73032b388efe2c778fc194bc81ed',
+  // ocDAI: '0x98cc3bd6af1880fcfda17ac477b2f612980e5e33',
   ocUSDC: '0x8ed9f862363ffdfd3a07546e618214b6d59f03d4'
 }
