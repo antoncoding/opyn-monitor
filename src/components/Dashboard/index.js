6f76121c10335079b0af46bf162d8453a50f08cc
Anton Cheng <anton@coolbitx.com>
2020-02-25 10:37:43 +0800

    Update go to token link

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,7 +7,7 @@ import { Header, DataView, IdentityBadge, Button } from '@aragon/ui'
 function DashBoard() {
     const history = useHistory();
     const goToToken = (addr) =>{
-      history.push(`/token/${addr}`)
+      history.push(`token/${addr}`)
     }
     return (
       <>
