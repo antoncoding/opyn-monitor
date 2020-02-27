c3785b4bf02eadf9de00fa63d13402af6dda9a4c
Anton Cheng <anton@coolbitx.com>
2020-02-27 11:46:50 +0800

    Adjust modal length

--- src/components/TokenView/ManageModal.js
+++ src/components/TokenView/ManageModal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { addETHCollateral, liquidate, getAccounts } from '../../utils/web3';
-import { getMaxLiquidatable } from '../../utils/infura';
+import { getMaxLiquidatable, getDecimals } from '../../utils/infura';
 
 import {
   Button,
@@ -15,6 +15,7 @@ import {
 
 function PositionModal({ tokenAddress, vaultOwner, collateral, isSafe, oTokensIssued, ratio }) {
   const [opened, setOpened] = useState(false);
+  const [decimals, setTokenDecimals] = useState(0)
   const [addValue, setAddValue] = useState(0);
   const [liquidateAmt, setLiquidateAmt] = useState(0);
   const open = () => setOpened(true);
@@ -25,14 +26,18 @@ function PositionModal({ tokenAddress, vaultOwner, collateral, isSafe, oTokensIs
       async function() {
         const accounts = await getAccounts();
         const maxLiquidatable = await getMaxLiquidatable(tokenAddress, vaultOwner, accounts[0]);
-        setLiquidateAmt(maxLiquidatable);
+        const _decimals = await getDecimals(tokenAddress)
+        setTokenDecimals(_decimals)
+        setLiquidateAmt(maxLiquidatable / 10 ** _decimals );
       }
   );
 
   return (
     <>
       <Button onClick={open} label='Manage'></Button>
-      <Modal width={800} padding={50} visible={opened} onClose={close}>
+      <Modal 
+        width={Math.min(window.innerWidth - 48, 750)}
+      padding={50} visible={opened} onClose={close}>
         <Box heading={'Owner'}>
           <IdentityBadge entity={vaultOwner} shorten={false} />
         </Box>
@@ -61,7 +66,7 @@ function PositionModal({ tokenAddress, vaultOwner, collateral, isSafe, oTokensIs
             type='number'
             adornment={<IconFundraising />}
             adornmentPosition='end'
-            value={liquidateAmt}
+            value={ liquidateAmt }
             onChange={(event) => {
               setLiquidateAmt(event.target.value);
             }}
@@ -70,7 +75,7 @@ function PositionModal({ tokenAddress, vaultOwner, collateral, isSafe, oTokensIs
             disabled={isSafe}
             label='Liquidate'
             onClick={() => {
-              liquidate(tokenAddress, vaultOwner, liquidateAmt);
+              liquidate(tokenAddress, vaultOwner, liquidateAmt * (10 ** decimals) );
             }}
           />
         </Box>
