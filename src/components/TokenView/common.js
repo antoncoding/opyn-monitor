3d55e066e4cbc3c0d6a1df01c6e129fcb9838908
Anton Cheng <anton@coolbitx.com>
2020-02-28 00:03:31 +0800

    Put renderList function in common

---
+++ src/components/TokenView/common.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import {
+  Tag,
+  IdentityBadge,
+} from '@aragon/ui';
+import VaultModal from './VaultModal';
+import ManageModal from './ManageModal'
+
+export const renderMyVaultRow = ({ owner, collateral, oTokensIssued, ratio, isSafe, oToken }) => {
+  return [
+    <IdentityBadge entity={owner} shorten={true} connectedAccount={true} />,
+    collateral,
+    oTokensIssued,
+    ratio,
+    createTag(isSafe, ratio),
+    <ManageModal
+      oToken={oToken}
+      owner={owner}
+      collateral={collateral}
+      isSafe={isSafe}
+      oTokensIssued={oTokensIssued}
+      ratio={ratio}
+    />,
+  ]
+}
+
+export const renderListEntry = ({ owner, collateral, oTokensIssued, ratio, isSafe, oToken }) => {
+  return [
+    <IdentityBadge entity={owner} shorten={true} />,
+    collateral,
+    oTokensIssued,
+    ratio,
+    createTag(isSafe, ratio),
+    <VaultModal
+      oToken={oToken}
+      owner={owner}
+      collateral={collateral}
+      isSafe={isSafe}
+      oTokensIssued={oTokensIssued}
+      ratio={ratio}
+    />,
+  ];
+}
+
+export const createTag = (isSafe, ratio) => {
+  return isSafe ? (
+    ratio < 1.7 ? (
+      <Tag background='#FFEBAD' color='#FEC100'>
+        {' '}
+        Danger{' '}
+      </Tag>
+    ) : (
+      <Tag mode='new'> safe </Tag>
+    )
+  ) : (
+    <Tag color='#E34343' background='#FFC6C6'>
+      Unsafe!
+    </Tag>
+  )
+}
