26253588921cf1d1b9e18cdf3ac34a4647fc99ea
Anton Cheng <anton@coolbitx.com>
2020-03-16 00:15:28 +0800

    Add icons

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -11,19 +11,19 @@ function HomePage() {
       <div style={{ padding: 5, opacity: 0.5 }}> Tools for DeFi Risk Management. </div>
       <div style={{ padding: '1%', display: 'flex', alignItems: 'center' }}>
         <div style={{ width: '30%', marginRight: '3%' }}>
-          <MainButton title='My Vaults' description='Manage collateral, Mint/Burn oTokens' onClick={()=>{
+          <MainButton title='My Vaults' description='Manage collateral, Mint/Burn oTokens' iconUrl='https://opyn.co/static/media/1.68813886.svg'  onClick={()=>{
             history.push('/myvaults')
           }} />
         </div>
 
         <div style={{ width: '30%' }}>
-          <MainButton title='All Contracts' description=' Monitor and liquidate vaults.' onClick={()=>{
+          <MainButton title='All Contracts' description=' Monitor and liquidate vaults.' iconUrl='https://opyn.co/static/media/2.18d22be0.svg'  onClick={()=>{
             history.push('/options/')
           }} />
         </div>
 
         <div style={{ width: '30%', marginLeft: '3%' }}>
-          <MainButton title='Trade' description='Buy, Sell or Provided Liquidity.' onClick={()=>{
+          <MainButton title='Trade' description='Buy, Sell or Provided Liquidity.' iconUrl='https://opyn.co/static/media/3.4da8b24d.svg'  onClick={()=>{
             history.push('./trade')
           }} />
         </div>
@@ -32,12 +32,14 @@ function HomePage() {
   );
 }
 
-function MainButton({ title, description, onClick }) {
+function MainButton({ title, description, iconUrl, onClick }) {
   return (
     <LinkBase onClick={onClick} style={{ width: '100%' }}>
       <Box>
-        <div style={{ fontSize: 18 }}>{title}</div>
+        <div style={{ padding:10, fontSize: 18 }}>{title}</div>
+        <img alt='icon' style={{ padding:10 }} src={iconUrl}></img>
         <div style={{ paddingTop: 5, opacity: 0.5 }}> {description} </div>
+        
       </Box>
     </LinkBase>
   );
