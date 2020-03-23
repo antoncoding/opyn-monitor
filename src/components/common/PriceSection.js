426faea0669af99709a32308f9d9a004acf1e309
Anton Cheng <anton@coolbitx.com>
2020-03-24 00:19:59 +0800

    Add forceDisply param

--- src/components/common/PriceSection.js
+++ src/components/common/PriceSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function PriceSection({ label, amt, symbol = '' }) {
-  if (parseFloat(amt) > 0) {
+function PriceSection({ label, amt, symbol = '', forceDisplay=false }) {
+  if (parseFloat(amt) > 0 || forceDisplay) {
     return (
       <div style={{ padding: 3, opacity: 0.5 }}>
         <span style={{ fontSize: 13 }}> {label} </span>{' '}
