a864c3ab89e187dc19152f85f685ce2074670b2f
Anton Cheng <anton@coolbitx.com>
2020-04-09 22:11:44 +0800

    ADD: Watch mode for my vaults

---
+++ src/components/MyVaults/NoWallet.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  TextInput, DataView, Button, LinkBase, EthIdenticon, useToast,
+} from '@aragon/ui';
+import { getPreference, storePreference } from '../../utils/storage';
+import { Comment } from '../common';
+import { isAddress } from '../../utils/number';
+
+function PleaseLogin({ setWatchAddress }) {
+  const toast = useToast();
+  const [InAddress, setAddress] = useState('');
+  const watch_addrs = getPreference('watch_addresses', '[]');
+  const usedAddresses = JSON.parse(watch_addrs);
+
+  return (
+    <>
+      {/* <Header></Header> */}
+      <Comment text="Please connect wallet to proceed or enter an address to use Watch Mode." />
+      <br />
+      <br />
+      <p style={{ fontSize: 20 }}> Watch Mode </p>
+      <Comment text="You won't be able to interact with any smart contract under Watch Mode." />
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: '50%' }}>
+          <TextInput
+            value={InAddress}
+            onChange={(e) => { setAddress(e.target.value); }}
+            wide
+          />
+
+          {usedAddresses.length > 0 ? (
+            <div style={{ paddingTop: '3%' }}>
+              <DataView
+                entries={usedAddresses}
+                fields={['used']}
+                entriesPerPage={5}
+                renderEntry={(address) => [
+                  <LinkBase onClick={() => {
+                    setWatchAddress(address);
+                    if (!usedAddresses.includes(address)) {
+                      usedAddresses.push(address);
+                      storePreference('watch_addresses', JSON.stringify(usedAddresses));
+                    }
+                  }}
+                  >
+                    {/* <IdentityBadge entity={address} /> */}
+                    <EthIdenticon
+                      address={address}
+                      scale={1.3}
+                      radius={5}
+                      soften={0.8}
+                    />
+                    <span style={{ paddingLeft: 8, fontSize: 15, fontFamily: 'aragon-ui-monospace' }}>{address}</span>
+                  </LinkBase>,
+                ]}
+              />
+            </div>
+          ) : <></>}
+        </div>
+        <div style={{ width: '8%', paddingLeft: '1%', paddingRight: '1%' }}>
+          <Button
+            label="Watch Address"
+            onClick={() => {
+              if (isAddress(InAddress)) {
+                setWatchAddress(InAddress);
+                if (!usedAddresses.includes(InAddress)) {
+                  usedAddresses.push(InAddress);
+                  storePreference('watch_addresses', JSON.stringify(usedAddresses));
+                }
+              } else {
+                toast('Invalid Address');
+              }
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+PleaseLogin.propTypes = {
+  // watchAddres: PropTypes.string.isRequired,
+  setWatchAddress: PropTypes.func.isRequired,
+};
+
+export default PleaseLogin;
