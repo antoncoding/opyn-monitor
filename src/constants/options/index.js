64adbce7dd9aff65ce74024ff79a5ad4d9c2ff82
Anton Cheng <anton@coolbitx.com>
2020-04-23 00:08:22 +0800

    Remove comment and trigger another build

--- src/constants/options/index.js
+++ src/constants/options/index.js
@@ -4,7 +4,7 @@ import { insurances } from './insurances';
 
 
 export const eth_options = eth_puts; // eth_calls.concat(eth_puts);
-export const allOptions = insurances.concat(eth_options); // .concat(eth_calls).concat(eth_puts);
+export const allOptions = insurances.concat(eth_options);
 export {
   eth_calls, eth_puts, insurances,
 };
