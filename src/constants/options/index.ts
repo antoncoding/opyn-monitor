a91ae5976eca5145b422f4b30bd0e2427f6a142b
Anton Cheng <antonassocareer@gmail.com>
2020-06-09 15:26:39 +0800

    Blacklist testing call

--- src/constants/options/index.ts
+++ src/constants/options/index.ts
@@ -5,6 +5,7 @@ import BigNumber from 'bignumber.js';
 export const blackList = [
   "0x67904e24b071d1406a63eceddea6ba0f4a79ab1e", // testing yDai insurance
   "0xc714cea4daaed7fbc66f936b69e79ec0ee998d93", // testing yDai insurance
+  "0xbaf6cfa199fbbe8a9e5198f2af20040c5e7b0333", // call
 ]
 
 export const defaultOption: optionWithStat = {
