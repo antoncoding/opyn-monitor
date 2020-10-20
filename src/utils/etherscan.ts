6e3e4ab0696a8da8e19572258a3f81a872588187
Anton Cheng <antonassocareer@gmail.com>
2020-10-20 22:57:32 +0800

    add backup for ETHPrice (#65)

--- src/utils/etherscan.ts
+++ src/utils/etherscan.ts
@@ -1,6 +1,22 @@
 const key = process.env.REACT_APP_ETHERSCAN_KEY
 
+export const getTokenPrice = async (addr) : Promise<string> => {
+  const url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${addr}&vs_currencies=usd`
+  const res = await fetch(url)
+  try {
+    return (await res.json())[addr].usd
+  } catch(error) {
+    console.error(`getTokenPrice error`, error)
+    return '0'
+  }
+}
+
 export const getETHPrice = async(): Promise<string> => {
   const res = await fetch(`https://api.etherscan.io/api?module=stats&action=ethprice&tag=latest&apikey=${key}`)
-  return (await res.json()).result.ethusd
+  try {
+    return (await res.json()).result.ethusd
+  } catch(error) {
+    return getTokenPrice('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+  }
+  
 }
